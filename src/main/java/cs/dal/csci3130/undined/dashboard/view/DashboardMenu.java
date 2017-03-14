4d7bd8ddf1e34ef086f3520bbe5ce1652f4f3960
Chico <xz528876@dal.ca>
2017-03-14 13:59:13 -0300

    1. persistence.xml file can be configured in build facets

--- src/main/java/cs/dal/csci3130/undined/dashboard/view/DashboardMenu.java
+++ src/main/java/cs/dal/csci3130/undined/dashboard/view/DashboardMenu.java
@@ -65,7 +65,7 @@ public final class DashboardMenu extends CustomComponent {
 		settings.addStyleName("user-menu");
 		final User user = getCurrentUser();
 		settingsItem = settings.addItem("", new ThemeResource("img/profile-pic-300px.jpg"), null);
-		updateUserName(null);
+		// updateUserName(null);
 		settingsItem.addItem("Edit Profile", new Command() {
 			@Override
 			public void menuSelected(MenuItem selectedItem) {
@@ -121,7 +121,7 @@ public final class DashboardMenu extends CustomComponent {
 	@Override
 	public void attach() {
 		super.attach();
-		updateNotificationsCount(null);
+		//updateNotificationsCount(null);
 	}
 	
 //	@Subscribe
