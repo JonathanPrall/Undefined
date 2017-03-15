cc0781dc5e716638896a8b5d7dfbf13bc59ab0c9
Chico <xz528876@dal.ca>
2017-03-15 08:54:27 -0300

    1. new backend added

---
+++ src/main/java/cs/dal/csci3130/undined/dashboard/DashboardUI.java
@@ -0,0 +1,17 @@
+package cs.dal.csci3130.undined.dashboard;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+@Theme("dashboard")
+@SuppressWarnings("serial")
+public class DashboardUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest request) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
