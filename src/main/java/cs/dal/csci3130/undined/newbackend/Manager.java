15c720c5c757211358a59b5f3a0cc6b7fc6988fd
Chico <xz528876@dal.ca>
2017-03-17 09:46:21 -0300

    1. add ManagerService
    					- Chico & Colton

--- src/main/java/cs/dal/csci3130/undined/newbackend/Manager.java
+++ src/main/java/cs/dal/csci3130/undined/newbackend/Manager.java
@@ -1,12 +1,20 @@
 package cs.dal.csci3130.undined.newbackend;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 public class Manager extends User {
 	
 	String R_ID;
-
+	
 	public Manager() {
 		this.setRole("manager");
 	}
+	
+	public Manager(Long id, String password, String role, String firstName, String lastName, String email, String phone, String R_ID) {
+		super(id, password, role, firstName, lastName, email, phone);
+		this.R_ID = R_ID;
+	}
+	
 	public String getR_ID() {
 		return R_ID;
 	}
@@ -15,4 +23,12 @@ public class Manager extends User {
 		R_ID = r_ID;
 	}
 	
+	@Override
+	public Manager clone() throws CloneNotSupportedException {
+		try {
+			return (Manager) BeanUtils.cloneBean(this);
+		} catch (Exception ex) {
+			throw new CloneNotSupportedException();
+		}
+	}
 }
---
+++ src/main/java/cs/dal/csci3130/undined/newbackend/ManagerService.java
@@ -0,0 +1,75 @@
+package cs.dal.csci3130.undined.newbackend;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// Backend service class. This is just a typical Java backend implementation
+// class and nothing Vaadin specific.
+public class ManagerService {
+
+    private static ManagerService instance;
+
+    private HashMap<Long, Manager> managers = new HashMap<>();
+    private long nextId = 0;
+
+    //I think this is the search box
+    public synchronized List<Manager> findAll(String stringFilter) {
+        ArrayList<Manager> arrayList = new ArrayList<Manager>();
+        
+        //For each manager
+        for (Manager manager : managers.values()) {
+            try {
+            	//If there is nothing in the search box
+            	//or
+            	//The Manager.toString() contains the text in the search box (not case sensitive)
+                boolean passesFilter = (stringFilter == null || stringFilter.isEmpty())
+                        || manager.toString().toLowerCase()
+                                .contains(stringFilter.toLowerCase());
+                //Add it to the list that will be returned
+                if (passesFilter) {
+                    arrayList.add(manager.clone());
+                }
+            
+            //If error, log it
+            } catch (CloneNotSupportedException ex) {
+                Logger.getLogger(ManagerService.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+        }
+        
+        //Sort the created list
+        Collections.sort(arrayList, new Comparator<Manager>() {
+            @Override
+            public int compare(Manager o1, Manager o2) {
+                return (int) (o1.getId() - o2.getId());
+            }
+        });
+        
+        //Return it
+        return arrayList;
+    }
+
+    public synchronized long count() {
+        return managers.size();
+    }
+
+    public synchronized void delete(Manager value) {
+        managers.remove(value.getId());
+    }
+
+    public synchronized void save(Manager entry) {
+        if (entry.getId() == null) {
+            entry.setId(nextId++);
+        }
+        try {
+            entry = (Manager) BeanUtils.cloneBean(entry);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        managers.put(entry.getId(), entry);
+    }
+
+}
